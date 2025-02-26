1e0fddd4542c741cb874a4b3b00221db54a849dd
aplekung <neuxbung77@gmail.com>
2025-02-26 21:37:55 +0700

    Update argon.js

--- js/argon.js
+++ js/argon.js
@@ -13,4 +13,18 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     });
+    document.querySelector(".Fix-btn").addEventListener("click", function() {
+        Swal.fire({
+            title: "ยืนยันการดาวน์โหลด",
+            text: "คุณต้องการไปยังหน้าดาวน์โหลดหรือไม่?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: "ใช่, ไปเลย!",
+            cancelButtonText: "ยกเลิก"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                window.open('argonfix.html', '_blank');
+            }
+        });
+    });
 });
